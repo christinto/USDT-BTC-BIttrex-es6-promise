0c0a5d5759b5387df7e9ce70b767f0c4aafe4d4e
Chris P <chrisperrymedia@gmail.com>
2017-11-07 15:33:23 +1100

    Create bitcoinPrice.js

---
+++ bitcoinPrice.js
@@ -0,0 +1,33 @@
+// write with express and and bootstrapped front end 
+const request = require('request');
+
+//Using USDT tether as proxy price for $USD.
+//https://bittrex.com/api/v1.1/public/getticker?market=USDT-BTC
+    return new Promise((resolve, reject) => {
+      var myCoin = encodeURIComponent(ticker);
+  
+      request({
+        url: `https://bittrex.com/api/v1.1/public/getticker?market=${myCoin}`,
+        json: true
+      }, (error, response, body) => {
+        if (error) {
+          reject('Unable to connect to Ubiq miner servers.');
+        } else if (body.result.Last != '') {
+          resolve({
+            
+            lastprice: body.result.Last,
+            bidPrice: body.result.Bid,
+            askPrice: body.result.Ask
+          });
+        }
+      });
+    });
+  };
+  
+      // console.log(JSON.stringify(result));
+      monero('USDT-BTC').then((lastprice) => {
+    console.log(JSON.stringify(lastprice));
+  }, (errorMessage) => { 
+    console.log(errorMessage);
+  });
+  
