7afdb59bfc2387896ebd8eb69612e87b766d64f4
christinto <chrisperrymedia@gmail.com>
2017-11-09 06:43:53 +0000

    Original promise with Express endpoint

--- bitcoinPrice.js
+++ bitcoinPrice.js
@@ -6,31 +6,75 @@ const request = require('request');
 const app = express();
 
 const port = 3035;
-/*
+/* We're res sending the promise response :)
 app.get('/', function(req, res) {
   res.send('SDAAFA');
 });
 */
 /* LIKE THIS
-//like the first example, call request
-request({
-  url: "https://blockchain.info/stats?format=json",
-  json: true
-}, function(error, response, body) {
-  ubqPayouts = body.paymentsTotal;
-});
-
-
+//except the whole promise sits inside app.get
 app.get("/", function(req, res) {
   res.send("Ubq miner payouts! " + ubqPayouts + " hell yeah");
 });
-
 */
 
+//Stays as is, doesn't weave into app. get..
+const createPriceResponse = (lastprice, res) => {
+  var response = {
+    lastBitcoinPrice: lastprice
+  };
+};
 
 //Using USDT tether as proxy price for $USD.
 //https://bittrex.com/api/v1.1/public/getticker?market=USDT-BTC
-//gonna try refashion resolve object lastprice: etc.. to var lastprice.
+//refashioned from resolve object lastprice: etc.. to a var lastprice.
+app.get('/', (req, res) => {
+ var bitcoin = (coin) => {
+    return new Promise((resolve, reject) => {
+      var encodedCoin = encodeURIComponent(coin);
+  
+      request({
+        url: `https://bittrex.com/api/v1.1/public/getticker?market=${encodedCoin}`,
+        json: true
+      }, (error, response, body) => {
+        if (error) {
+          reject('Unable to connect to Bittrex servers.');
+        } else if (body.result.Last != '') {
+          /*resolve({
+             lastprice: body.result.Last
+            //bidPrice: body.result.Bid,
+            //askPrice: body.result.Ask
+          });
+          */
+          resolve(
+            lastprice = body.result.Last
+          );
+        }
+      });
+    });
+  };
+  
+      // console.log(JSON.stringify(result));
+          //CREATE RESPONSE FOR A VAR HERE FOR ENDPOINT DISPLAY?
+          //Passing in a variable lastprice = or key lastprice: is the same :)
+      bitcoin('USDT-BTC').then((lastprice) => {
+    console.log(JSON.stringify(lastprice));
+    createPriceResponse(lastprice, res);
+  }, (errorMessage) => { 
+    console.log(errorMessage);
+  });
+}); //app.get end  
+
+
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });
+
+
+/* Original promise
+//Using USDT tether as proxy price for $USD.
+//https://bittrex.com/api/v1.1/public/getticker?market=USDT-BTC
+//refashioned from resolve object lastprice: etc.. to a var lastprice.
 var bitcoin = (coin) => {
     return new Promise((resolve, reject) => {
       var encodedCoin = encodeURIComponent(coin);
@@ -42,15 +86,14 @@ var bitcoin = (coin) => {
         if (error) {
           reject('Unable to connect to Bittrex servers.');
         } else if (body.result.Last != '') {
-          //resolve({
-          resolve(
-            lastprice = body.result.Last
-            
-            //lastprice: body.result.Last,
-            //bidPrice: body.result.Bid,
-            //askPrice: body.result.Ask
-          //});
-          );
+          resolve({
+            lastprice: body.result.Last
+            bidPrice: body.result.Bid,
+            askPrice: body.result.Ask
+          });
+          //resolve(
+            //lastprice = body.result.Last
+          //);
         }
       });
     });
@@ -59,16 +102,8 @@ var bitcoin = (coin) => {
       // console.log(JSON.stringify(result));
       bitcoin('USDT-BTC').then((lastprice) => {
     console.log(JSON.stringify(lastprice));
-    //CREATE RESPONSE FOR A VAR HERE FOR ENDPOINT DISPLAY?
   }, (errorMessage) => { 
     console.log(errorMessage);
   });
+*/
   
-
-app.get("/", function(req, res) {
-  res.send(lastprice);
-});
-
-  app.listen(port, () => {
-    console.log('We are live on ' + port);
-  });
